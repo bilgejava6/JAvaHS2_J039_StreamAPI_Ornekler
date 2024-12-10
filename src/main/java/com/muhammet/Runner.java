0132b870586ff9930e0a9b3bddeb06c266253595
bilgejava6 <bilgejava6@gmail.com>
2024-12-10 20:17:51 +0300

    Soru 1 ve 2

--- src/main/java/com/muhammet/Runner.java
+++ src/main/java/com/muhammet/Runner.java
@@ -1,7 +1,39 @@
 package com.muhammet;
 
+import com.muhammet.entity.Kullanici;
+
+import static com.muhammet.utility.Veri.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Runner {
     public static void main(String[] args) {
-
+        /**
+         * Temel Seviye Sorular
+         */
+        /**
+         * 1. Aktif Kullanıcıları filtreleyerek bir başka listeye aktarın ve bu listeyi yazdırın
+         * Cevap:
+         *         List<Kullanici> aktifKullanicilar = null;
+         *
+         *         // Java17+ aktifKullanicilar = Veri.getKullaniciList().stream().filter(k-> k.isAktiflik()).toList();
+         *         // Java17 öncesi aktifKullanicilar = Veri.getKullaniciList().stream().filter(k-> k.isAktiflik()).collect(Collectors.toList());
+         *
+         *         aktifKullanicilar = Veri.getKullaniciList().stream().filter(Kullanici::isAktiflik).toList();
+         *
+         *         System.out.println(aktifKullanicilar);
+         */
+        /**
+         * 2. Kullanıcı listesinde var olan kullanıcıların adların başka bir listeye aktarıldığı ve bu ad listesinin
+         * ekrana yazıldığı kodu yazınız.
+         * CEVAP:
+         *
+         *       List<String> adList = getKullaniciList().stream().map(Kullanici::getAd).toList();
+         *         System.out.println(adList);
+         */
+        /**
+         * 3.
+         */
     }
 }
