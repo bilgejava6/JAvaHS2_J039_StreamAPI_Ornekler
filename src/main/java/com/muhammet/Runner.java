136683541acc49c43433735956f7da36c4f13481
bilgejava6 <bilgejava6@gmail.com>
2024-12-10 21:28:34 +0300

    Soru 3,4,5,6,7

--- src/main/java/com/muhammet/Runner.java
+++ src/main/java/com/muhammet/Runner.java
@@ -4,8 +4,12 @@ import com.muhammet.entity.Kullanici;
 
 import static com.muhammet.utility.Veri.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Runner {
     public static void main(String[] args) {
@@ -33,7 +37,52 @@ public class Runner {
          *         System.out.println(adList);
          */
         /**
-         * 3.
+         * 3. Ankara da yaşan kullanıcıların listesini çıkartıp ekrana yazdırınız.
+         * CEVAP:
+         * List<Kullanici> ankaraKullanicilari = getKullaniciList().stream().filter(k-> k.getSehir().equals("Ankara")).toList();
+         *         System.out.println(ankaraKullanicilari);
          */
+        /**
+         * 4. Yaşı 18 ve üzeri olan 65 yaşından küçük olanları listele
+         * CEVAP:
+         *
+         *         List<Kullanici> calisabilirKulalnicilar = null;
+         *         Long calisabilirKullaniciSayisi = 0L;
+         *         Stream<Kullanici> kullaniciStream = getKullaniciList().stream().filter(k-> k.getYas()>17 && k.getYas()<65);
+         *         // calisabilirKulalnicilar = kullaniciStream.toList();
+         *         calisabilirKullaniciSayisi = kullaniciStream.count();
+         */
+        /**
+         * 5. Kullanıcı adı A ile başlayan kullanıcıların listesini yazdırınız.
+         * CEVAP:
+         *         List<Kullanici> aIleBaslayanlar = getKullaniciList().stream().filter(k-> k.getAd().startsWith("A")).toList();
+         *         System.out.println(aIleBaslayanlar);
+         */
+        /**
+         * 6. Kullanıcıların yaşlarının toplamı nedir?
+         * CEVAP:
+         *         int yasToplami = getKullaniciList().stream().mapToInt(Kullanici::getYas).sum();
+         *         OptionalDouble yasOrtalamasi = getKullaniciList().stream().mapToInt(Kullanici::getYas).average();
+         *         System.out.println("kullanıcı yaş toplamı....: "+ yasToplami);
+         *         yasOrtalamasi.ifPresent(System.out::println);
+         *         yasOrtalamasi.ifPresent(o->System.out.println((int)o));
+         */
+        /**
+         * 7. Kullanıcıların yaşlarına göre sıralanmasını istiyorum ancak ilk 3 kişiyi listelemeniz gerekli.
+         * İPUCU: sorted, limit
+         * CEVAP:
+         *
+         *         List<Kullanici> enKucuk3Kullanici = getKullaniciList().stream()
+         *                 .sorted(Comparator.comparingInt(Kullanici::getYas))
+         *                 .limit(3)
+         *                 .toList();
+         *         System.out.println(enKucuk3Kullanici);
+         */
+        /**
+         *
+         */
+
+
+
     }
 }
